4bb88e3d21234ccaef39efb344d8d1b64cd35a7f
Avinash Rath <avinash.rrath@gmail.com>
2019-10-01 23:46:18 +0530

    add BorderShadow style

---
+++ lib/utils/styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+List<BoxShadow> boxShadows = [
+  BoxShadow(blurRadius: 7.0, offset: Offset(-7, -7), color: Colors.grey[300]),
+  BoxShadow(blurRadius: 7.0, offset: Offset(7, -7), color: Colors.grey[300]),
+  BoxShadow(blurRadius: 7.0, offset: Offset(-7, 7), color: Colors.grey[300]),
+  BoxShadow(blurRadius: 7.0, offset: Offset(7, 7), color: Colors.grey[300]),
+];
